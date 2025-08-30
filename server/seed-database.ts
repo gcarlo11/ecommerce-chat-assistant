c69590de9bd4d7cffa85507aceac9796d7b521d1
gcarlo11 <ridhorasyid676@gmail.com>
2025-08-30 18:09:40 +0700

    feat(server): implement vector search index creation and enhance database setup logic

--- server/seed-database.ts
+++ server/seed-database.ts
@@ -7,7 +7,7 @@ import { MongoDBAtlasVectorSearch } from "@langchain/mongodb"
 import { z } from "zod"; //for schema validation
 import  "dotenv/config" //to read .env file
 
-const Client = new MongoClient(process.env.MONGODB_ATLAS_URL as string) //connect to MongoDB Atlas
+const client = new MongoClient(process.env.MONGODB_ATLAS_URL as string) //connect to MongoDB Atlas
 
 const llm = new ChatGoogleGenerativeAI({ //initialize the LLM
     model: "gemini-1.5-flash", //model name
@@ -45,3 +45,45 @@ const itemsSchema = z.object({ //defining the schema for the data we want from A
 type Item = z.infer<typeof itemsSchema> //TypeScript type for the schema
 
 const parser = StructuredOutputParser.fromZodSchema(z.array(itemsSchema)) //create a parser using the schema
+
+async function setupDatabaseAndCollection(): Promise<void> {
+    console.log("Setting up database and collection...")
+        const db = client.db("inventory_database")
+        const collections = await db.listCollections({ name: "items" }).toArray()
+
+        if (collections.length === 0) {
+            await db.createCollection("items")
+            console.log("Created 'items' collection ini 'inventory_database' database/")
+        } else {
+            console.log("'items' collection already exists in 'inventory_database' database.")  
+        }  
+}
+
+async function createVectorSearchIndex(): Promise<void> {
+    try{
+        const db = client.db("inventory_database")
+        const collection = db.collection("items")
+        await collection.dropIndexes() //remove existing indexes to avoid conflicts
+        const vectorSearchIdx= {
+            name: "vector_index",
+            type: "vectorSearch",
+            definition: {
+                "fields": [
+                    {
+                        "type": "vector",
+                        "path": "embedding",
+                        "numDimensions": 768,
+                        "similarity": "cosine"
+                    }
+                ]
+            }
+        }
+        console.log("Creating vector search index...")
+        await collection.createSearchIndex(vectorSearchIdx);
+        console.log("Vector search index created successfully.")
+        
+    } catch (error) {
+        console.error("Error creating vector search index:", error) 
+    }
+
+}
